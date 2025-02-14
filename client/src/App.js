f0d15150f47fd80dcc81b3530de6a168425e1cee
Luca Haller <lucahhaller@gmail.com>
2025-02-14 00:45:48 -0300

    VioletChange

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import Navbar from "./components/Navbar";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import logo3 from "../src/assets/logos/logo3.png";
+import logo4 from "../src/assets/fondos/4.png";
 import fondo from "../src/assets/fondos/huesitos2.jpg";
+import fondo2 from "../src/assets/fondos/fonprueba.png";
 import { useEffect } from "react";
 import ftm from "../src/assets/pictures/FTM.jpeg";
 
@@ -23,44 +25,47 @@ function App() {
   }, []);
 
   return (
-    <div className=" bg-gradient-to-r from-stone-300 to-stone-400">
+    <div
+      className=" bg-gradient-to-r bg-black
+     to-stone-400"
+    >
       <Navbar />
       <section
-        className="pt-28 px-10 pb-72 min-h-screen bg-gradient-to-r from-stone-300 to-stone-400 flex justify-center"
+        className="pt-28 px-10 pb-72 min-h-screen bg-gradient-to-r flex justify-center"
         id="first"
       >
         <div
-          className="w-full xl:h-[1000px]  flex-col bg-marroncito items-center border-8 rounded-3xl p-0 shadow-lg flex bg-cover"
-          style={{ backgroundImage: `url(${fondo})` }}
+          className="w-full xl:h-[1000px]  flex-col bg-purple-700 items-center  border-purple-400 border-8 rounded-3xl p-0 shadow-lg flex bg-cover"
+          style={{ backgroundImage: `url(${fondo2})` }}
         >
           <img
-            src={logo3}
+            src={logo4}
             alt="Logo"
             className="w-fit h-3/4 pb-0"
             data-aos="zoom-in"
           />
-          <div className="flex flex-row gap-10 ">
-            <button className="px-10 py-3 border-4 text-3xl border-black shadow-[3px_3px_0px_#000] text-marronclaro hover:text-marroncito font-bold bg-marroncito transition-all duration-300 cursor-pointer hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#000] hover:bg-marronclaro active:translate-x-[3px] active:translate-y-[3px] active:shadow-none">
+          <div className="flex flex-row gap-10  md:text-3xl text-xl">
+            <button className="px-10 py-3 border-4  border-black shadow-[3px_3px_0px_#000] text-marronclaro hover:text-marroncito font-bold bg-violet-500 transition-all duration-300 cursor-pointer hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#000] hover:bg-marronclaro active:translate-x-[3px] active:translate-y-[3px] active:shadow-none">
               Chart
             </button>
-            <button className="px-7 py-3 border-4 text-3xl border-black shadow-[3px_3px_0px_#000] hover:text-marronclaro font-bold bg-marronclaro transition-all duration-300 cursor-pointer hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#000] hover:bg-marronhover active:translate-x-[3px] active:translate-y-[3px] active:shadow-none">
+            <button className="px-7 py-3 border-4  border-black shadow-[3px_3px_0px_#000] hover:text-marronclaro font-bold bg-violetita transition-all duration-300 cursor-pointer hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#000] hover:bg-marronhover active:translate-x-[3px] active:translate-y-[3px] active:shadow-none">
               Join us
             </button>
           </div>
         </div>
       </section>
       <section className="min-h-full flex justify-center items-center py-20">
-        <div className="max-w-7xl w-full flex flex-col lg:flex-row items-center  justify-center lg:items-start gap-10">
+        <div className="max-w-7xl w-full flex flex-col lg:flex-row items-center  p-4 md:p-4 lg:p-0 justify-center lg:items-start gap-10">
           {/* Contenido a la izquierda */}
           <div className="lg:w-2/3 text-center lg:text-left">
             <h2 className="text-7xl font-lucky text-stone-800">
               About the history of{" "}
-              <span className="text-red-600 inline-block" data-aos="flip-up">
+              <span className="text-purple-800 inline-block" data-aos="flip-up">
                 CONAN
               </span>
             </h2>
 
-            <p className="mt-5 text-xl text-stone-600">
+            <p className="mt-5 text-xl text-stone-600 text-center md:text-justify">
               Conan is more than just a memecoin. It was born from the vision of
               a strong and brave dog, ready to conquer the crypto world. A token
               with a unique story and a community-driven spirit.
@@ -69,11 +74,16 @@ function App() {
 
           {/* Imagen a la derecha */}
           <div className="lg:w-1/2 flex justify-center " data-aos="fade-left">
-            <img
-              src={ftm}
-              alt="Conan Illustration"
-              className="w-80 h-auto rotate-6 shadow-lg rounded-lg"
-            />
+            <div className="relative p-1  rotate-6 bg-gradient-to-r from-purple-800 to-cyan-400 rounded-xl">
+              <div className="p-2 rounded-xl  ">
+                {/* Contenido dentro del borde */}
+                <img
+                  src={ftm}
+                  alt="Conan Illustration"
+                  className="w-80 h-auto shadow-lg rounded-lg "
+                />
+              </div>
+            </div>
           </div>
         </div>
       </section>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,30 +1,20 @@
 import { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
-import logo from "../assets/logos/LOGO_2.png";
-import AOS from "aos";
-import "aos/dist/aos.css";
-AOS.init({
-  duration: 1000,
-  delay: 100,
-  once: true,
-});
+import logo from "../assets/fondos/2.png";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="text-white py-4 px-6 fixed w-full top-0 z-50 bg-gradient-to-r from-stone-300 to-stone-400">
-      <div
-        className="max-w-7xl mx-auto flex justify-between items-center relative "
-        data-aos="zoom-in-up"
-      >
+    <nav className="text-white py-4 px-6 fixed w-full top-0 z-50">
+      <div className="max-w-7xl mx-auto flex justify-between items-center relative">
         {/* LOGO */}
         <a href="#" className="text-7xl font-bold text-yellow-500">
           <img src={logo} alt="Logo" className="h-20 w-auto" />
         </a>
 
         {/* LINKS DESKTOP CENTRADOS */}
-        <ul className="hidden md:flex space-x-8 text-lg absolute  font-lucky bg-marroncito py-3 px-7 border-4 border-marronclaro rounded-full left-1/2 transform -translate-x-1/2 text-stone-200">
+        <ul className="hidden md:flex space-x-8 text-lg absolute font-lucky bg-violet-500 py-3 px-7 border-4 border-black rounded-full left-1/2 transform -translate-x-1/2 text-stone-300">
           <li>
             <a href="#inicio" className="hover:text-marronclaro">
               Home
@@ -60,7 +50,7 @@ export default function Navbar() {
         {/* BOTÓN CERRAR CON MAYOR Z-INDEX */}
 
         {/* LINKS */}
-        <div className="flex flex-col items-center space-y-6 text-2xl  font-lucky text-stone-200">
+        <div className="flex flex-col items-center space-y-6 text-2xl font-lucky">
           <a
             href="#inicio"
             onClick={() => setMenuOpen(false)}
