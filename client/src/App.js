8c1c6259b6a90dee492fea82b811f316b0ad2482
Luca Haller <lucahhaller@gmail.com>
2025-02-14 14:06:47 -0300

    ChangeColors

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,10 @@ import "aos/dist/aos.css";
 import logo3 from "../src/assets/logos/logo3.png";
 import logo4 from "../src/assets/fondos/4.png";
 import fondo from "../src/assets/fondos/huesitos2.jpg";
-import fondo2 from "../src/assets/fondos/fonprueba.png";
+import fondo2 from "../src/assets/fondos/fondo3.png";
 import { useEffect } from "react";
-import ftm from "../src/assets/pictures/FTM.jpeg";
+import ftm from "../src/assets/pictures/fotomiley.png";
+import Joinus from "./pages/Joinus";
 
 function App() {
   useEffect(() => {
@@ -20,7 +21,7 @@ function App() {
     AOS.init({
       duration: 1000,
       delay: 100,
-      once: true,
+      once: false,
     });
   }, []);
 
@@ -35,7 +36,7 @@ function App() {
         id="first"
       >
         <div
-          className="w-full xl:h-[1000px]  flex-col bg-purple-700 items-center  border-purple-400 border-8 rounded-3xl p-0 shadow-lg flex bg-cover"
+          className="w-full xl:h-[1000px]  flex-col bg-purple-800 items-center  border-purple-400 border-8 rounded-3xl p-0 shadow-lg flex bg-cover"
           style={{ backgroundImage: `url(${fondo2})` }}
         >
           <img
@@ -44,11 +45,14 @@ function App() {
             className="w-fit h-3/4 pb-0"
             data-aos="zoom-in"
           />
-          <div className="flex flex-row gap-10  md:text-3xl text-xl">
-            <button className="px-10 py-3 border-4  border-black shadow-[3px_3px_0px_#000] text-marronclaro hover:text-marroncito font-bold bg-violet-500 transition-all duration-300 cursor-pointer hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#000] hover:bg-marronclaro active:translate-x-[3px] active:translate-y-[3px] active:shadow-none">
+          <div
+            className="flex flex-row gap-10  md:text-3xl text-xl"
+            data-aos="flip-up"
+          >
+            <button className="px-10 py-3 border-4  border-black shadow-[3px_3px_0px_#000] text-white hover:text-gray-300 font-bold bg-violet-600 transition-all duration-300 cursor-pointer hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#000] hover:bg-violet-700 active:translate-x-[3px] active:translate-y-[3px] active:shadow-none">
               Chart
             </button>
-            <button className="px-7 py-3 border-4  border-black shadow-[3px_3px_0px_#000] hover:text-marronclaro font-bold bg-violetita transition-all duration-300 cursor-pointer hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#000] hover:bg-marronhover active:translate-x-[3px] active:translate-y-[3px] active:shadow-none">
+            <button className="px-7 py-3 border-4  text-gray-900 border-black shadow-[3px_3px_0px_#000] hover:text-gray-800 font-bold bg-violetita transition-all duration-300 cursor-pointer hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#000] hover:bg-pink-500 active:translate-x-[3px] active:translate-y-[3px] active:shadow-none">
               Join us
             </button>
           </div>
@@ -58,18 +62,29 @@ function App() {
         <div className="max-w-7xl w-full flex flex-col lg:flex-row items-center  p-4 md:p-4 lg:p-0 justify-center lg:items-start gap-10">
           {/* Contenido a la izquierda */}
           <div className="lg:w-2/3 text-center lg:text-left">
-            <h2 className="text-7xl font-lucky text-stone-800">
+            <h2 className="text-7xl font-lucky text-stone-200">
               About the history of{" "}
-              <span className="text-purple-800 inline-block" data-aos="flip-up">
+              <span className="text-violet-600 inline-block" data-aos="flip-up">
                 CONAN
               </span>
             </h2>
 
-            <p className="mt-5 text-xl text-stone-600 text-center md:text-justify">
+            <p className="mt-5 text-xl text-stone-400 text-center md:text-justify">
               Conan is more than just a memecoin. It was born from the vision of
               a strong and brave dog, ready to conquer the crypto world. A token
               with a unique story and a community-driven spirit.
             </p>
+            <div
+              className="flex flex-row gap-10 mt-10  md:text-3xl text-xl"
+              data-aos="flip-up"
+            >
+              <button className="px-10 py-3 border-4 border-gray-100 shadow-[3px_3px_0px_#fff] text-marronclaro hover:text-gray-300 font-bold bg-violet-600 transition-all duration-300 cursor-pointer hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#fff] hover:bg-violet-800 active:translate-x-[3px] active:translate-y-[3px] active:shadow-none">
+                Read all history
+              </button>
+              <button className="px-7 py-3 border-4 text-gray-900 border-gray-100 shadow-[3px_3px_0px_#fff] hover:text-gray-800 font-bold bg-violetita transition-all duration-300 cursor-pointer hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#fff] hover:bg-pink-500 active:translate-x-[3px] active:translate-y-[3px] active:shadow-none">
+                See a video
+              </button>
+            </div>
           </div>
 
           {/* Imagen a la derecha */}
@@ -87,6 +102,7 @@ function App() {
           </div>
         </div>
       </section>
+      <Joinus />
     </div>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
-import logo from "../assets/fondos/2.png";
+import logo from "../assets/fondos/23.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init({
+  duration: 1000,
+  delay: 100,
+  once: true,
+});
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className="text-white py-4 px-6 fixed w-full top-0 z-50">
-      <div className="max-w-7xl mx-auto flex justify-between items-center relative">
+      <div
+        className="max-w-7xl mx-auto flex justify-between items-center relative"
+        data-aos="flip-up"
+      >
         {/* LOGO */}
         <a href="#" className="text-7xl font-bold text-yellow-500">
           <img src={logo} alt="Logo" className="h-20 w-auto" />
         </a>
 
         {/* LINKS DESKTOP CENTRADOS */}
-        <ul className="hidden md:flex space-x-8 text-lg absolute font-lucky bg-violet-500 py-3 px-7 border-4 border-black rounded-full left-1/2 transform -translate-x-1/2 text-stone-300">
+        <ul className="hidden md:flex space-x-8 text-lg absolute font-lucky bg-violet-500 py-3 px-7 border-4 border-black rounded-full left-1/2 transform -translate-x-1/2 text-stone-100">
           <li>
             <a href="#inicio" className="hover:text-marronclaro">
               Home
